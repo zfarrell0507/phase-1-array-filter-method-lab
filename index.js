0a905b1ce4c832cc49b651bb908c7e47de536c83
Zachary Farrell <zfarrell0507@gmail.com>
2022-07-21 14:08:08 -0400

    commit tree 553f8b93bf8ac53f007e7324336888956709a3dc

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(source, string) {
+    return source.filter((driver) => driver.toLowerCase() === string.toLowerCase());
+}
+  
+function fuzzyMatch(source, testString) {
+    return source.filter((driver) => driver.toLowerCase().indexOf(testString.toLowerCase()) === 0);
+}
+  
+function matchName(source, theName) {
+    return source.filter((record) => record.name === theName);
+}
+
